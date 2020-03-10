3185077c4d259c0177e7307cabda5ab10099c2eb
Roberto Jose De Lavor Rodrigues <r.de.lavor.rodrigues@avanade.com>
2020-03-10 14:14:30 -0300

    Included adding product php file

---
+++ _add_product.php
@@ -0,0 +1,14 @@
+<?php 
+
+	$code = $_POST['edtCode'];
+	$description = $_POST['edtDescription'];
+	$quantity = $_POST['edtQuantity'];
+	$category = $_POST['cbbCategory'];
+	$manufacturer = $_POST['cbbManufacturer'];
+
+	echo $description;
+
+	
+
+
+ ?>
